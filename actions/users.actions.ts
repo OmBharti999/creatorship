4c470f8773948ef72c283c48cc9ce72473dbed7c
om_official <ombharti888@gmail.com>
2024-07-23 01:03:50 +0530

    update schema

--- actions/users.actions.ts
+++ actions/users.actions.ts
@@ -6,15 +6,15 @@ import { UserJSON } from "@clerk/nextjs/server";
 export const createNewUser = async (userWebhookParams: UserJSON) => {
   try {
     const {
-      firstName,
-      lastName: lastname,
+      first_name,
+      last_name: lastname,
       id,
-      primaryEmailAddress,
+      email_addresses,
     } = Object(userWebhookParams);
     const user = await prisma.user.create({
       data: {
-        email: primaryEmailAddress?.emailAddress || "",
-        firstname: firstName || "",
+        email: email_addresses[0]?.email_address || "",
+        firstname: first_name || "",
         clerk_id: id,
         lastname,
       },
