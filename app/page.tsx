e24c9bf820a31b3e80432918a60f469c56566d9e
om_official <ombharti888@gmail.com>
2024-07-22 22:47:23 +0530

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/shared/navbar";
 import { Sidebar } from "@/components/shared/sidebar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -5,17 +6,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center space-y-12 p-24">
-      <h2 className="text-4xl">Select Your Role</h2>
-      <Sidebar />
-      <div className="gap-5 flex ">
-        <Button asChild>
-          <Link href={`/creator`}>Creator</Link>
-        </Button>
-        <Button>
-          <Link href={`/business-owner`}>Business Owner</Link>
-        </Button>
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center space-y-12 p-24">
+        <h2 className="text-4xl">Select Your Role</h2>
+        <Sidebar />
+        <div className="gap-5 flex ">
+          <Button asChild>
+            <Link href={`/creator`}>Creator</Link>
+          </Button>
+          <Button>
+            <Link href={`/business-owner`}>Business Owner</Link>
+          </Button>
+        </div>
+      </main>
+    </>
   );
 }
---
+++ components/shared/navbar/index.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { Factory } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="h-10 bg-black w-full flex items-center px-6">
+      <h3 className="flex gap-3">
+        <Factory size={20} />
+        Creatorship.io
+      </h3>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+};
+
+export default Navbar;
