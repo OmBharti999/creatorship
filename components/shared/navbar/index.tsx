8dd653aad16a9c284ce50025fba88d632257bbf1
om_official <ombharti888@gmail.com>
2024-07-23 02:16:45 +0530

    udpate navbar

--- components/shared/navbar/index.tsx
+++ components/shared/navbar/index.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="h-10 bg-black w-full flex items-center px-6">
-      <h3 className="flex gap-3 text-white">
+    <header className="h-10 bg-black w-full flex items-center justify-between px-6 lg:px-10 ">
+      <h3 className="flex gap-3 text-white ">
         <Factory size={20} />
         Creatorship.io
       </h3>
